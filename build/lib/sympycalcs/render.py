9597e0d17e61d3e10f74e936c5dd55f77dddb0df
PascalGitz <pascal.gitz@hotmail.ch>
2023-04-03 20:45:27 +0200

    Render mit fakultativen renders

--- build/lib/sympycalcs/render.py
+++ build/lib/sympycalcs/render.py
@@ -1,46 +1,32 @@
-def eq_render(variables, parameters=False):
-    """Reads all Variables in the current cell. Then displays them as an sympy equation with it's lefthandside as the variable name,
-    and the righthandside of the input without substitution. The righthandside is converted via Sympify which does not handle all inputs!
-    It then displays the Equation with it's substitution. If the parameters are given, it also displays the Equation with substituted parameters. 
-
+def eq_render(variables, parameters=False, raw_input = True, units=False):
+    
+    
+    
+    """Reads variables in current cell. Displays them as sympy equations.
 
     Args:
-        variables (dict): locals() has to be put in!
-        parameters (dict, optional): A dictionary containing all the parameters for the subsitution. Defaults to False.
-
-    Returns:
-        list: List of Equations with substituted parameters.
+        variables (dict): locals() or globals(). Containing left- and righthandside of the Equation.
+        parameters (dict): Substitute all the equations with the given Parameters.
+        raw_input (bool, optional): If True an attempt is made of rendering the unsubstituted input via simpify. 
+        Parts that cannot be sympified will not get displayed! Defaults to False.
+        units (list): An attempt is made of converting the numeric value to the given units. Each index of the list corresponds to a variable in the cell.
     """
+    
+    #Functionimport
     import io
     from contextlib import redirect_stdout
     from sympy import Eq, Symbol, sympify
 
     offset=0
-
+    local_vars = variables
     ipy = get_ipython()
     out = io.StringIO()
 
     with redirect_stdout(out):
         ipy.run_line_magic("history", format(ipy.execution_count - offset))
 
-    #process each line...
-    x = out.getvalue().replace(" ", "").split("\n")
+
     
-    try:
-        Eq_raw_rhs = [a.split("=")[1].replace("sp.", "").replace("u.","") for a in x if "=" in a]
-        Eq_raw_lhs = [a.split("=")[0] for a in x if "=" in a]
-        
-        Eq_raws = []
-        for i in range(0,len(Eq_raw_rhs)):
-            Eq_raw = Eq(Symbol(Eq_raw_lhs[i]), sympify(Eq_raw_rhs[i]))
-            Eq_raws.append(Eq_raw)
-    except:
-        pass
-
-    x = [a.split("=")[0] for a in x if "=" in a] #all of the variables in the cell
-
-    g = variables
-
     # Class for equations with its parts
     class equation:
         def __init__(self, diction) -> None:
@@ -50,40 +36,71 @@ def eq_render(variables, parameters=False):
             pass
 
 
-    subs = equation({k:g[k] for k in x if k in g})    
-
-    ## raw equations
-    Eq_subs = []
-    for i in range(0,len(subs.symbols)):
-        Eq_sub = Eq(Symbol(f'{subs.symbols[i]}'), subs.values[i])
-        Eq_subs.append(Eq_sub)
+    #process each line...
+    each_line = out.getvalue().replace(" ", "").split("\n")
     
-    #substitution with parameters
-    Eq_params = []
-    for i in range(0,len(subs.symbols)):
-        if parameters!=False:
-            Eq_param = Eq(Symbol(f'{subs.symbols[i]}'), subs.values[i]).subs(parameters)
-            Eq_params.append(Eq_param)
+    # save the the variable name and input as a string for left and righthandside
+    Eq_raw_rhs = [a.split("=")[1].replace("sp.", "").replace("u.","") for a in each_line if "=" in a]
+    Eq_raw_lhs = [a.split("=")[0] for a in each_line if "=" in a]
+
+    #Create normal Equation 
+    subs = equation({k:local_vars[k] for k in Eq_raw_lhs if k in local_vars})    
 
 
+
+
+    # Add all Equations to a list and iterate trough it. It guarantees that the order of the Equations is correct.
     Eq_tot =[]   
-    for i in range(0,len(Eq_subs)):
-        Eq_tot.append(Eq_raws[i])
+
+    for i in range(0,len(subs.symbols)):
+
+        # Raw input Equations with sympify
+        if raw_input != False:
+            try:
+                Eq_raws = []
+                for j in range(0,len(Eq_raw_rhs)):
+                    Eq_raw = Eq(Symbol(Eq_raw_lhs[j]), sympify(Eq_raw_rhs[j]))
+                    Eq_raws.append(Eq_raw)
+            except:
+                pass
+            Eq_tot.append(Eq_raws[i])
+        
+
+        # Normal Equation with Variablename and its substitution
+        Eq_subs = []
+        for j in range(0,len(subs.symbols)):
+            Eq_sub = Eq(Symbol(f'{subs.symbols[j]}'), subs.values[j])
+            Eq_subs.append(Eq_sub)
         Eq_tot.append(Eq_subs[i])
+        
+        # Equation with substituted Parameters
         if parameters!=False:
+            from sympy.physics.units import convert_to
+            Eq_params = []
+            for j in range(0,len(subs.symbols)):
+                    Eq_param = Eq(Symbol(f'{subs.symbols[j]}'), subs.values[j]).subs(parameters).simplify().evalf(3)
+                    try:
+                        Eq_params.append(convert_to(Eq_param, units[j]))
+                    except:
+                        Eq_params.append(Eq_param)
+        
             Eq_tot.append(Eq_params[i])
 
-
+            
+    
+    ## Removing duplicates
     Eq_tot = list(dict.fromkeys(Eq_tot))
-    # print(Eq_tot)
+
+    ## Display the Each Equation
     for Eq in Eq_tot:
         display(Eq)
+    
 
-    Eq_params_rhs = []
-    for Eq in Eq_params:
-        Eq_params_rhs.append(Eq.rhs)
-
-    return Eq_params_rhs
+    if parameters!=False:
+        Eq_params_rhs =[]
+        for Eq in Eq_params:
+            Eq_params_rhs.append(Eq.rhs)
+        return tuple(Eq_params_rhs)
 
 
 
