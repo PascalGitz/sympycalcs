d00ed61efcc57e1e9be25d9a9dae3d1b531b5c9b
PascalGitz <pascal.gitz@hotmail.ch>
2023-09-25 19:08:16 +0200

    moved all functions to helpers

--- sympycalcs/helpers.py
+++ sympycalcs/helpers.py
@@ -19,7 +19,6 @@ def Eq_subs(target_eq, *substitution_eqs):
     eq_3 = sp.Eq(c, t * k)
 
     eq_1_subs = Eq_subs(eq_1, eq_1, eq_2, eq_3)
-    print(eq_1_subs)  # Output: a = q*t*k*v + t*q*v + t*k
     """
     result_eq = target_eq
     previous_result_eq = None
@@ -31,3 +30,165 @@ def Eq_subs(target_eq, *substitution_eqs):
                 result_eq = result_eq.subs(eq.lhs, eq.rhs)
                 
     return result_eq
+
+
+def param_value(input_dict, base_units=None):
+    """
+    Convert a dictionary containing parameters with units to a unitless dictionary in base SI units.
+
+    Args:
+        input_dict (dict): Parameters with units.
+        base_units (list of sympy.physics.units.*): List of base SI units. Defaults to [N, m, second].
+
+    Returns:
+        dict: Parameters without units in base SI units.
+
+    Example:
+    input_dict = {'mass': 5 * kg, 'distance': 3 * m, 'time': 2 * s}
+    base_units = [N, m, s]
+
+    result = param_value(input_dict, base_units)
+    # Output: {'mass': 5, 'distance': 3, 'time': 2}
+    """
+    from sympy.physics.units import convert_to
+    from sympy import Mul
+
+    if base_units is None:
+        base_units = [N, m, second]  # Default base SI units
+
+    result_dict = {}
+
+    for key, value in input_dict.items():
+        converted_value = convert_to(value, base_units)
+
+        if isinstance(converted_value, Mul):
+            numeric_factor = converted_value.args[0]
+        else:
+            numeric_factor = converted_value
+
+        result_dict[key] = numeric_factor
+
+    return result_dict
+
+
+def dict_render(params):
+    """renders a dictionary containing the parameters
+
+    Args:
+        params (dict): Parameters for substitution
+    """
+    from sympy import Eq, Symbol
+    symbols = list(params.keys())
+    values = list(params.values())
+
+    for i in range(0,len(symbols)):
+        display(Eq(symbols[i], values[i]))   
+
+
+
+def eq_pretty_units(equation, unit=None):
+    """
+    Format a SymPy equation with a specified unit and return it as a LaTeX string.
+    
+    Args:
+        equation (sympy.Eq): The SymPy equation to format.
+        unit (str): The unit string to append to the equation.
+        equation_number (int): Optional equation number for LaTeX numbering.
+
+    Returns:
+        str: LaTeX-formatted equation with the specified unit.
+    """
+    if unit == None:
+        units = latex(equation.rhs.subs(equation.rhs.args[0], 1))
+    else:
+        units = fr'\mathrm{{{unit}}}'
+        
+        
+    formatted_equation_latex_sympy = latex(Eq(equation.lhs, equation.rhs.args[0]))
+    formatted_equation_latex = (
+        fr"\begin{{align}}{formatted_equation_latex_sympy} \, {units} \end{{align}}" 
+    )
+    return Latex(formatted_equation_latex)
+
+
+
+
+def dict_to_table(d):
+    """
+    Creates a markdown table out of a dict with two rows and sorts the keys alphabetically.
+    """
+    from sympy import Eq, Symbol, latex
+    from IPython.display import display, Markdown
+    
+    def eq_pretty_units(equation):
+        """
+        Format a SymPy equation with a specified unit and return it as a LaTeX string.
+
+        Args:
+            equation (sympy.Eq): The SymPy equation to format.
+
+        Returns:
+            str: LaTeX-formatted equation with the specified unit.
+        """
+        units = latex(equation.rhs.subs(equation.rhs.args[0], 1))
+
+        formatted_equation_latex_sympy = latex(Eq(equation.lhs, equation.rhs.args[0]))
+        formatted_equation_latex = (
+            fr"${formatted_equation_latex_sympy} \, {units}$" 
+        )
+        return formatted_equation_latex
+
+    sorted_items = sorted(d.items(), key=lambda item: str(item[0]))  # Schlüssel alphabetisch sortieren
+    n = len(sorted_items)
+    table = "|   |   |\n|---|---|\n"
+    for i, (key, value) in enumerate(sorted_items):
+        if isinstance(key, str):
+            key_sym = Symbol(key)  # Benutzerdefiniertes Symbol aus der Zeichenkette erstellen
+        else:
+            key_sym = key  # Verwende den Schlüssel direkt, wenn es bereits ein Symbol ist
+        
+        # Call eq_pretty_units to format the equation with units
+        formatted_equation = eq_pretty_units(Eq(key_sym, value))
+        
+        table += f"| {formatted_equation} "
+        if i % 2 == 1:
+            table += "|\n"
+    if n % 2 == 1:
+        table += "| |\n"
+    display(Markdown(table))
+
+
+def eq_display(*args):
+    """
+Simple display of an sympy equation for as many args as desired. First entry Eq1.lhs and second entry is Eq1.rhs, third entry creates Eq2.lhs and fourth entry Eq2.rhs and so on.
+    """
+    from sympy import sympify, Eq
+    from IPython.display import display
+
+
+    for i in range(0, len(args), 2):
+        lhs = sympify(args[i]) if isinstance(args[i], str) else args[i]
+        rhs = sympify(args[i+1]) if isinstance(args[i+1], str) else args[i+1]
+        display(Eq(lhs,rhs))
+
+
+def pdf_to_svg(dir, delete_original=False):
+    import os
+    import subprocess
+
+    inkscape_path = r'C:\Program Files\Inkscape\bin\inkscape.exe'
+    input_dir = dir
+    output_dir = dir
+
+    for filename in os.listdir(input_dir):
+        if filename.endswith(".pdf"):
+            input_file = os.path.join(input_dir, filename)
+            output_file = os.path.join(output_dir, os.path.splitext(filename)[0] + ".svg")
+            subprocess.run([inkscape_path, "--export-type=svg", "--pdf-poppler", "-l", f'--export-filename={output_file}', input_file])
+
+            if delete_original:
+                os.remove(input_file)
+
+
+
+
