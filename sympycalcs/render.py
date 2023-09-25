37a1ada60e85cd2290804b5c68b87e91eb7947dc
PascalGitz <pascal.gitz@hotmail.ch>
2023-09-25 18:59:51 +0200

    dict_to_table with pretty units combined

--- sympycalcs/render.py
+++ sympycalcs/render.py
@@ -13,48 +13,6 @@ def dict_render(params):
 
 
 
-def dict_to_table(d):
-    """
-Creates a markdown table out of a dict with two rows and sorts the keys alphabetically.
-    """
-    from sympy import Eq, Symbol, latex
-    from IPython.display import display, Markdown
-    
-    sorted_items = sorted(d.items(), key=lambda item: str(item[0]))  # Schlüssel alphabetisch sortieren
-    n = len(sorted_items)
-    table = "|   |   |\n|---|---|\n"
-    for i, (key, value) in enumerate(sorted_items):
-        if isinstance(key, str):
-            key_sym = Symbol(key)  # Benutzerdefiniertes Symbol aus der Zeichenkette erstellen
-        else:
-            key_sym = key  # Verwende den Schlüssel direkt, wenn es bereits ein Symbol ist
-        
-        table += f"| ${latex(Eq(key_sym, value))}$ "
-        if i % 2 == 1:
-            table += "|\n"
-    if n % 2 == 1:
-        table += "| |\n"
-    display(Markdown(table))
-
-
-
-
-def eq_display(*args):
-    """
-Simple display of an sympy equation for as many args as desired. First entry Eq1.lhs and second entry is Eq1.rhs, third entry creates Eq2.lhs and fourth entry Eq2.rhs and so on.
-    """
-    from sympy import sympify, Eq
-    from IPython.display import display
-
-
-    for i in range(0, len(args), 2):
-        lhs = sympify(args[i]) if isinstance(args[i], str) else args[i]
-        rhs = sympify(args[i+1]) if isinstance(args[i+1], str) else args[i+1]
-        display(Eq(lhs,rhs))
-
-
-
-
 def eq_pretty_units(equation, unit=None):
     """
     Format a SymPy equation with a specified unit and return it as a LaTeX string.
@@ -78,4 +36,72 @@ def eq_pretty_units(equation, unit=None):
         fr"\begin{{align}}{formatted_equation_latex_sympy} \, {units} \end{{align}}" 
     )
     return Latex(formatted_equation_latex)
+
+
+
+
+def dict_to_table(d):
+    """
+    Creates a markdown table out of a dict with two rows and sorts the keys alphabetically.
+    """
+    from sympy import Eq, Symbol, latex
+    from IPython.display import display, Markdown
+    
+    def eq_pretty_units(equation):
+        """
+        Format a SymPy equation with a specified unit and return it as a LaTeX string.
+
+        Args:
+            equation (sympy.Eq): The SymPy equation to format.
+
+        Returns:
+            str: LaTeX-formatted equation with the specified unit.
+        """
+        units = latex(equation.rhs.subs(equation.rhs.args[0], 1))
+
+        formatted_equation_latex_sympy = latex(Eq(equation.lhs, equation.rhs.args[0]))
+        formatted_equation_latex = (
+            fr"${formatted_equation_latex_sympy} \, {units}$" 
+        )
+        return formatted_equation_latex
+
+    sorted_items = sorted(d.items(), key=lambda item: str(item[0]))  # Schlüssel alphabetisch sortieren
+    n = len(sorted_items)
+    table = "|   |   |\n|---|---|\n"
+    for i, (key, value) in enumerate(sorted_items):
+        if isinstance(key, str):
+            key_sym = Symbol(key)  # Benutzerdefiniertes Symbol aus der Zeichenkette erstellen
+        else:
+            key_sym = key  # Verwende den Schlüssel direkt, wenn es bereits ein Symbol ist
+        
+        # Call eq_pretty_units to format the equation with units
+        formatted_equation = eq_pretty_units(Eq(key_sym, value))
+        
+        table += f"| {formatted_equation} "
+        if i % 2 == 1:
+            table += "|\n"
+    if n % 2 == 1:
+        table += "| |\n"
+    display(Markdown(table))
+
+
+
+
+
+def eq_display(*args):
+    """
+Simple display of an sympy equation for as many args as desired. First entry Eq1.lhs and second entry is Eq1.rhs, third entry creates Eq2.lhs and fourth entry Eq2.rhs and so on.
+    """
+    from sympy import sympify, Eq
+    from IPython.display import display
+
+
+    for i in range(0, len(args), 2):
+        lhs = sympify(args[i]) if isinstance(args[i], str) else args[i]
+        rhs = sympify(args[i+1]) if isinstance(args[i+1], str) else args[i+1]
+        display(Eq(lhs,rhs))
+
+
+
+
 
