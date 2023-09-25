eea81cbe711caea425cc2ea30493e1502e4089a2
PascalGitz <pascal.gitz@hotmail.ch>
2023-09-25 19:13:16 +0200

    Added docstrings and optimized

--- sympycalcs/helpers.py
+++ sympycalcs/helpers.py
@@ -72,42 +72,65 @@ def param_value(input_dict, base_units=None):
 
 
 def dict_render(params):
-    """renders a dictionary containing the parameters
+    """
+    Render a dictionary containing parameter substitutions as SymPy equations.
 
     Args:
-        params (dict): Parameters for substitution
+        params (dict): Parameters for substitution, where keys are symbols and values are numeric values.
+        
+    Example:
+    params = {'a': 3, 'b': 5, 'c': 7}
+    
+    dict_render(params)
+    # Displays:
+    # a = 3
+    # b = 5
+    # c = 7
     """
     from sympy import Eq, Symbol
+    from IPython.display import display
+
     symbols = list(params.keys())
     values = list(params.values())
 
-    for i in range(0,len(symbols)):
-        display(Eq(symbols[i], values[i]))   
+    for i in range(len(symbols)):
+        display(Eq(Symbol(symbols[i]), values[i]))
+
 
 
 
 def eq_pretty_units(equation, unit=None):
     """
-    Format a SymPy equation with a specified unit and return it as a LaTeX string.
-    
+    Format a SymPy equation with an optional specified unit and return it as a LaTeX string.
+
     Args:
         equation (sympy.Eq): The SymPy equation to format.
-        unit (str): The unit string to append to the equation.
-        equation_number (int): Optional equation number for LaTeX numbering.
+        unit (str, optional): The unit string to append to the equation. Default is None.
 
     Returns:
-        str: LaTeX-formatted equation with the specified unit.
+        str: LaTeX-formatted equation with the specified unit (if provided).
+
+    Example:
+    equation = Eq(a, b + c)
+    
+    # Without unit
+    latex_equation = eq_pretty_units(equation)
+    # Output: '\\begin{align}a = b + c\\end{align}'
+    
+    # With unit
+    latex_equation = eq_pretty_units(equation, unit='m/s^2')
+    # Output: '\\begin{align}a = b + c\\, \\mathrm{m/s^2}\\end{align}'
     """
-    if unit == None:
+    from sympy import Eq, latex, Latex
+
+    if unit is None:
         units = latex(equation.rhs.subs(equation.rhs.args[0], 1))
     else:
         units = fr'\mathrm{{{unit}}}'
         
-        
     formatted_equation_latex_sympy = latex(Eq(equation.lhs, equation.rhs.args[0]))
-    formatted_equation_latex = (
-        fr"\begin{{align}}{formatted_equation_latex_sympy} \, {units} \end{{align}}" 
-    )
+    formatted_equation_latex = fr"\begin{{align}}{formatted_equation_latex_sympy} \, {units} \end{{align}}"
+    
     return Latex(formatted_equation_latex)
 
 
@@ -115,7 +138,16 @@ def eq_pretty_units(equation, unit=None):
 
 def dict_to_table(d):
     """
-    Creates a markdown table out of a dict with two rows and sorts the keys alphabetically.
+    Create a markdown table from a dictionary with two rows and sort the keys alphabetically.
+
+    Args:
+        d (dict): Dictionary containing equations or values.
+
+    Example:
+    d = {'a': Eq(x, y), 'b': 42, 'c': Eq(z, w)}
+    
+    dict_to_table(d)
+    # Displays a formatted markdown table of the dictionary entries.
     """
     from sympy import Eq, Symbol, latex
     from IPython.display import display, Markdown
@@ -133,19 +165,17 @@ def dict_to_table(d):
         units = latex(equation.rhs.subs(equation.rhs.args[0], 1))
 
         formatted_equation_latex_sympy = latex(Eq(equation.lhs, equation.rhs.args[0]))
-        formatted_equation_latex = (
-            fr"${formatted_equation_latex_sympy} \, {units}$" 
-        )
+        formatted_equation_latex = fr"${formatted_equation_latex_sympy} \, {units}$" 
         return formatted_equation_latex
 
-    sorted_items = sorted(d.items(), key=lambda item: str(item[0]))  # Schlüssel alphabetisch sortieren
+    sorted_items = sorted(d.items(), key=lambda item: str(item[0]))  # Sort keys alphabetically
     n = len(sorted_items)
     table = "|   |   |\n|---|---|\n"
     for i, (key, value) in enumerate(sorted_items):
         if isinstance(key, str):
-            key_sym = Symbol(key)  # Benutzerdefiniertes Symbol aus der Zeichenkette erstellen
+            key_sym = Symbol(key)  # Create a custom symbol from the string
         else:
-            key_sym = key  # Verwende den Schlüssel direkt, wenn es bereits ein Symbol ist
+            key_sym = key  # Use the key directly if it's already a symbol
         
         # Call eq_pretty_units to format the equation with units
         formatted_equation = eq_pretty_units(Eq(key_sym, value))
@@ -158,36 +188,63 @@ def dict_to_table(d):
     display(Markdown(table))
 
 
+
 def eq_display(*args):
     """
-Simple display of an sympy equation for as many args as desired. First entry Eq1.lhs and second entry is Eq1.rhs, third entry creates Eq2.lhs and fourth entry Eq2.rhs and so on.
+    Simple display of a SymPy equation. Takes pairs of arguments where the first argument is the left-hand side (LHS)
+    of the equation, and the second argument is the right-hand side (RHS).
+
+    Args:
+        *args: Variable-length argument list of LHS and RHS pairs for equations.
+
+    Example:
+    eq_display('x', '2*x + 3')
+    # Displays the equation x = 2*x + 3.
     """
     from sympy import sympify, Eq
     from IPython.display import display
 
-
     for i in range(0, len(args), 2):
         lhs = sympify(args[i]) if isinstance(args[i], str) else args[i]
-        rhs = sympify(args[i+1]) if isinstance(args[i+1], str) else args[i+1]
-        display(Eq(lhs,rhs))
+        rhs = sympify(args[i + 1]) if isinstance(args[i + 1], str) else args[i + 1]
+        display(Eq(lhs, rhs))
 
 
-def pdf_to_svg(dir, delete_original=False):
+def pdf_to_svg(input_dir, output_dir=None, delete_original=False, inkscape_path=None):
+    """
+    Convert PDF files in the specified directory to SVG format using Inkscape.
+
+    Args:
+        input_dir (str): The directory containing the PDF files to be converted.
+        output_dir (str, optional): The directory where the SVG files will be saved.
+            If None, SVG files will be saved in the same directory as the input PDF files.
+        delete_original (bool, optional): Whether to delete the original PDF files after conversion. Default is False.
+        inkscape_path (str, optional): The path to the Inkscape executable. If not provided, a default path is used.
+
+    Example:
+    pdf_to_svg('pdf_files', output_dir='svg_files', delete_original=True, inkscape_path='/path/to/inkscape')
+    # Converts PDF files in 'pdf_files' directory to SVG format, saves in 'svg_files', and deletes the original PDFs.
+    """
     import os
     import subprocess
 
-    inkscape_path = r'C:\Program Files\Inkscape\bin\inkscape.exe'
-    input_dir = dir
-    output_dir = dir
+    if inkscape_path is None:
+        # Default Inkscape executable path (update this to your Inkscape installation path)
+        inkscape_path = r'C:\Program Files\Inkscape\bin\inkscape.exe'
+
+    if output_dir is None:
+        output_dir = input_dir
 
     for filename in os.listdir(input_dir):
         if filename.endswith(".pdf"):
             input_file = os.path.join(input_dir, filename)
             output_file = os.path.join(output_dir, os.path.splitext(filename)[0] + ".svg")
+            
             subprocess.run([inkscape_path, "--export-type=svg", "--pdf-poppler", "-l", f'--export-filename={output_file}', input_file])
 
             if delete_original:
                 os.remove(input_file)
+
 
 
 
